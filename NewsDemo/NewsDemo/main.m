5c72213474cd7aa8215438ab47c8890d1fbdf693
daling <sun409377708@163.com>
2016-10-25 15:39:06 +0800

    创建项目

---
+++ NewsDemo/NewsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsDemo
+//
+//  Created by maoge on 16/10/25.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
